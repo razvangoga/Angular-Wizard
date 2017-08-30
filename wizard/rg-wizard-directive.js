e9ddea1a35740c452ec9ecb18e1586f00b1caa79
Razvan Goga <razvan.goga@gmail.com>
2017-08-30 12:22:19 +0200

    functional wizard with customizable pages - first draft

--- wizard/rg-wizard-directive.js
+++ wizard/rg-wizard-directive.js
@@ -4,9 +4,41 @@ var rgWizardDirective = {
         return {
             restrict: 'E',
             replace: true,
-            transclude:true,
+            transclude: true,
             templateUrl: '/wizard/rg-wizard-directive-template.html',
-            controller: function() {return {}}
+            controller: function () {
+                return {
+                }
+            },
+            link: function (scope, elem, attrs) {
+                scope.wizardName = attrs.wizardName;
+
+                scope.steps = [];
+
+                var setCurrentStep = function (index) {
+                    scope.currentStepIndex = index;
+                    scope.currentStep = scope.steps[index];
+                };
+
+                scope.registerStep = function(stepName) {
+                    scope.steps.push(stepName);
+                    setCurrentStep(0);
+                };
+
+                scope.onMovePrevious = function () {
+                    if (scope.currentStepIndex === 0)
+                        return;
+
+                    setCurrentStep(scope.currentStepIndex - 1);
+                };
+
+                scope.onMoveNext = function () {
+                    if (scope.currentStepIndex === scope.steps.length - 1)
+                        return;
+
+                    setCurrentStep(scope.currentStepIndex + 1);
+                };
+            }
         };
     }
 };
--- wizard/rg-wizard-step-directive.js
+++ wizard/rg-wizard-step-directive.js
@@ -12,6 +12,13 @@ var rgWizardStepDirective = {
             templateUrl: '/wizard/rg-wizard-step-directive-template.html',
             link: function (scope, elem, attrs) {
                 scope.name = attrs.stepName;
+                
+                scope.$parent.registerStep(scope.name);
+
+                scope.canShow = function() {
+                    var show = scope.name === scope.$parent.currentStep;
+                    return show;
+                }
             }
         };
     }
